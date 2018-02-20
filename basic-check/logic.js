bc6c0e4211dae75d3360c1cb2eae3eb19639f4e1
Duvanchi <cochize0@gmail.com>
2018-02-20 16:27:07 -0500

    Added city & state input auto-fill on zipcode entry, search function fired on enter keypress & submit button click,grayed out text in city & state fields

--- basic-check/logic.js
+++ basic-check/logic.js
@@ -1,5 +1,38 @@
+// Stores input values in patron object globally
 var patron = {};
 
+// function to fill in city & state fields upon successful entry of a valid zip code
+$(document).ready(function() {
+    $("#city").hide();
+    $("#state").hide();
+    $("#cityLabel").hide();
+    $("#stateLabel").hide();
+    $("#zipCode").keyup(function() {
+        var zip = $(this);
+
+        // AJAX call to retrieve city & state info from Arcgis
+        // data is then  spilt and placed into correct fields
+        if (zip.val().length === 5) {
+            $.ajax({
+                url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text=" + zip.val() + "&f=pjson",
+                cache: false,
+                dataType: "json",
+                type: "GET",
+                success: function(result, success) {
+                    var parsedData = result.locations[0].name.split(', ');
+                    console.log(parsedData);
+                    $("#city").val(parsedData[1]).show();
+                    $("#state").val(parsedData[2]).show();
+                    $("#cityLabel").show();
+                    $("#stateLabel").show();
+                    console.log(result);
+                }
+            });
+        }
+    });
+});
+
+// Captures user info from input fields & clears them upon form submittal
 function captureInfo() {
     
     patron.address = $("#address").val().trim(),
@@ -8,17 +41,24 @@ function captureInfo() {
     patron.state = $("#state").val().trim() 
     console.log(patron);
     userDetails();
+    $(".userInput").val('');
+    $("#city").hide();
+    $("#state").hide();
+    $("#cityLabel").hide();
+    $("#stateLabel").hide();
 };
 
- function userDetails(address, city, state, zip) {
+// Converts user input into useable query string for Arcgis API
+function userDetails(address, city, state, zip) {
     var urlAddress = patron.address.split(' ').join("+");
 	var urlCity = patron.city.split(' ').join("+");
-	var urlState = patron.state;
+	var urlState = patron.state.split(' ').join("+");
     var urlZip = patron.zipCode;
     
     var addressString = urlAddress + "%2C+" + urlCity + "%2C+" + urlState + "+" + urlZip;
     console.log(addressString);
 
+    // AJAX call to retrieve lat & long coordinates
     $.ajax({
 		type: "GET",
 		url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find?text=" + addressString + "&f=pjson",
@@ -30,10 +70,12 @@ function captureInfo() {
             console.log(xCoords);
 			console.log(yCoords);
 
+            // Additional AJAX call to check whether sepcified address lies within correct bounndary
 			$.ajax({
 				url: "https://gisweb.townofchapelhill.org/arcgis/rest/services/CorporateData/JurisdictionalLimits/MapServer/0/query?geometry=" + xCoords + "%2C" + yCoords + "&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelWithin&returnGeometry=false&outSR=102100&returnCountOnly=true&f=json"	
 			}).done(function(response) {
                 console.log(response);
+                // 1 for yes, 0 for no
                 if (response.count === 0) {
                     alert("Address not within specified boundary");
                 } else {
@@ -44,5 +86,12 @@ function captureInfo() {
 	  	dataType: "json"
 	});
 };
+
+// Allows captureInfo() & userDetails() to be fired upon pressing of enter button
+$(document).keypress(function(e) {
+    if (e.which === 13) {
+        captureInfo();
+    };
+});
 
 
